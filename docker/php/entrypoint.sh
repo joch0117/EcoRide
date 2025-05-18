4adfc9ca184f962995e815b83146b350c76d9b38
joch 0117 <jochaigneau1@gmail.com>
2025-05-18 15:30:08 +0200

    create admin

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 
-php-fpm &
-nginx -g "daemon off;"
+# Si on nous passe une commande, on l’exécute directement
+if [ "$#" -gt 0 ]; then
+    exec "$@"
+fi
 
+# Sinon on lance le stack classique PHP-FPM + Nginx
+php-fpm &
+exec nginx -g "daemon off;"
