5f1f1ec64f322cfa60cc8c9044f948cf3663b2a3
joch 0117 <jochaigneau1@gmail.com>
2025-05-19 21:25:54 +0200

    erreur

--- app/public/index.php
+++ app/public/index.php
@@ -1,8 +1,16 @@
 <?php
-
+use Symfony\Component\HttpFoundation\Request;
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+file_put_contents('/tmp/request_headers.log', json_encode($_SERVER)."\n", FILE_APPEND);
+Request::setTrustedProxies(
+    ['0.0.0.0/0', '::/0'],
+    Request::HEADER_X_FORWARDED_FOR |
+                        Request::HEADER_X_FORWARDED_HOST |
+                        Request::HEADER_X_FORWARDED_PROTO |
+                        Request::HEADER_X_FORWARDED_PORT
+);
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
