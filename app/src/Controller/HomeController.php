9f9f29574b663c189daa3ebc326898dfcc125cff
joch 0117 <jochaigneau1@gmail.com>
2025-05-19 20:12:55 +0200

    erreur corrige prod

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +10,9 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(LoggerInterface $logger): Response
     {
+        $logger->error('Test log erreur!');
         return $this->render('home/index.html.twig');
     }
 }
