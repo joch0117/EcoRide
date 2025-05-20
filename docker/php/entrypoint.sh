6a3768b3189dfe07a6a475e4b9b7c5a1c553e05c
joch 0117 <jochaigneau1@gmail.com>
2025-05-20 11:24:23 +0200

    ajout volume persistant

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -6,6 +6,9 @@ if [ "$#" -gt 0 ]; then
     exec "$@"
 fi
 
+if [ ! -f /var/www/html/public/uploads/users/default-profile.png ]; then
+    cp /var/www/html/public/uploads/users/default-profile.png.bak /var/www/html/public/uploads/users/default-profile.png
+fi
 
 php-fpm &
 exec nginx -g "daemon off;"
