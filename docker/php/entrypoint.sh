90663c4764c5c0cadc930f3dc78642628aed0fa8
joch 0117 <jochaigneau1@gmail.com>
2025-05-27 17:49:56 +0200

    entrypoint.sh modifier création volume

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 set -e
 
+# 1. Création automatique des dossiers nécessaires
+mkdir -p /var/www/html/public/uploads/users
+chown -R www-data:www-data /var/www/html/public/uploads
+chmod -R 775 /var/www/html/public/uploads
+
 
 if [ "$#" -gt 0 ]; then
     exec "$@"
 fi
+
 
 if [ ! -f /var/www/html/public/uploads/users/default-profile.png ]; then
     if [ -f /var/www/html/public/uploads/users/default-profile.png.bak ]; then
@@ -13,6 +19,7 @@ if [ ! -f /var/www/html/public/uploads/users/default-profile.png ]; then
         echo "INFO: default-profile.png.bak not found, skipping default photo copy"
     fi
 fi
+
 
 php-fpm &
 exec nginx -g "daemon off;"
