427ce24f2cd6803d44d70b693e8d18e1a9514a13
joch 0117 <jochaigneau1@gmail.com>
2025-06-09 13:23:07 +0200

    test mongodb distant + mise a jour readme +commande create admin en dev

---
+++ app/src/Command/CreateAdminCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(
+    name: 'app:create-admin',
+    description: 'Créer un compte administrateur pour EcoRide',
+)]
+class CreateAdminCommand extends Command
+{
+    public function __construct(
+        private EntityManagerInterface $em,
+        private UserPasswordHasherInterface $passwordHasher,
+    ) {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $user = new User();
+        $user->setEmail('admin-ecoride@ecoride.com');
+        $user->setUsername('admin');
+        $user->setRoles(["ROLE_ADMIN"]); 
+
+        $hashedPassword = $this->passwordHasher->hashPassword($user,'Helium355538!');
+        $user->setPassword($hashedPassword);
+
+        $this->em->persist($user);
+        $this->em->flush();
+
+        $io->success('Compte administrateur créé avec succès !');
+
+        return Command::SUCCESS;
+    }
+}
